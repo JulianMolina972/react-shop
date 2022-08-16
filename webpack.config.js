bedd669fc2a563a4ba89289332895822fc3c13bd
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-15 21:30:40 -0500

    Styles sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const webpack = require('html-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -28,6 +28,14 @@ module.exports = {
             loader: 'html-loader',
           }
         ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ]
       }
     ]
   },
@@ -35,6 +43,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
     })
-  ]
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3005,
+  }
 }
