2943f6c6766908cd35353ff98aff7eb4b7f6d23f
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-16 12:24:50 -0500

    components, cotainers, pages structure and composition with styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { ProductList }  from '../containers/ProductList';
 
 export const Home = () => {
-  return (
-    <div>
-      <h1>React shop</h1>
-    </div>
-  )
+	return (
+		<>
+			<ProductList />
+		</>
+	);
 }
+
+
