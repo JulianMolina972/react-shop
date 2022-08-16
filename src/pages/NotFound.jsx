12c8a678479c90fc411fcab35afa9c99e882b8a0
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-16 10:08:40 -0500

    routes and styles

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/NotFound.scss'
+
+export const NotFound = () => {
+  return (
+    <div className='not-found'>
+      <h2>
+        Not Found, error 404
+      </h2>
+    </div>
+  )
+}
